c59f25eebe64cff707bc4a5492d6b6bc92b66579
Norbert Bodziony <norbertbodziony@gmail.com>
2021-02-05 15:36:12 +0100

    remove copy

--- tests copy/oracle_custormer.js
+++
@@ -1,8 +0,0 @@
-const anchor = require('@project-serum/anchor')
-const assert = require('assert')
-describe('oracle', () => {
-  const provider = anchor.Provider.local()
-  anchor.setProvider(provider)
-
-  const program = anchor.workspace.Oracle
-})
